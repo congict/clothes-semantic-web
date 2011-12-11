56e51537cb0cde35141fbf1fecbea5b7427e212a
Nuno Khan <nunok7@gmail.com>
2011-12-11 22:03:25 +0000

    working on authentication

---
+++ Clothes/src/java/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import util.User;
+
+/**
+ *
+ * @author psychok7
+ */
+public class UserDAO {
+
+    public User authenticate(String email, String password) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}

2af0a27bf4622f7ef436c30713e93d6344be92c3
Nuno Khan <nunok7@gmail.com>
2012-01-07 22:13:24 +0000

    populatingjena working

--- ClothesWS/src/java/util/PopulateOntology.java
+++ ClothesWS/src/java/util/PopulateOntology.java
@@ -7,10 +7,10 @@ package util;
 import com.hp.hpl.jena.ontology.*;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,51 +27,54 @@ public class PopulateOntology {
         model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
         model.read("file:clothes.owl");
         init();
+        saveOWL();
     }
-    private void init(){
+
+    private void init() {
         createShoes();
-       // createPants();
+        // createPants();
     }
 
     private void createShoes() {
-        OntClass shoes = model.getOntClass(NamedSpace+"Shoes");
-        Individual shoe0 = model.createIndividual(NamedSpace + "shoe0", shoes);
-        Property hasSize =  model.getProperty(NamedSpace+"Size");
-        Property hasColour =  model.getProperty(NamedSpace+"Colour");
+        OntClass shoes = model.getOntClass(NamedSpace + "Shoes");
+        OntClass size = model.getOntClass(NamedSpace + "Size");
+        OntClass color = model.getOntClass(NamedSpace + "Color");
+        OntClass material = model.getOntClass(NamedSpace + "Material");
 
-      //  Restriction anonR = model.createRestriction( hasSize );
-      //  anonR.asCardinalityRestriction().setCardinality(1);
-        
-        
-        
-        shoe0.addLiteral(hasSize, "45");
-        shoe0.addLiteral(hasColour, "blacknigga");
+
+        Individual PatentLeatherOxford = model.createIndividual(NamedSpace + "Patent_Leather_Oxford", shoes);
+
+        Property hasSize = model.getProperty(NamedSpace + "hasSize");
+        Property hasColor = model.getProperty(NamedSpace + "hasColor");
+        Property hasMaterial = model.getProperty(NamedSpace + "isMadeOf");
+
+        PatentLeatherOxford.addProperty(hasSize, model.createIndividual(NamedSpace + "38", size));
+        PatentLeatherOxford.addProperty(hasColor, model.createIndividual(NamedSpace + "blue", color));
+        PatentLeatherOxford.addProperty(hasMaterial, model.createIndividual(NamedSpace + "cotton", material));
+
+        iterateStatement(PatentLeatherOxford, hasColor);
+//
+    }
+
+    private void createPants() {
+    }
+
+    
+    
+    private void iterateStatement(Individual ind, Property prop) {
+        StmtIterator x = ind.listProperties(prop);
+
+        while (x.hasNext()) {
+            System.out.println("teste" + x.next());
+        }
+    }
+
+    private void saveOWL() {
         try {
             model.write(new FileOutputStream(new File("clothes.owl")));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(PopulateOntology.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        System.out.println("teste: " + shoe0.getPropertyValue(hasSize));
-    }
-
-    private void createPants() {
-        OntClass pants = model.getOntClass("Pants");
-        
-        for (Iterator i = pants.listSubClasses(); i.hasNext(); ) {
-            OntClass c = (OntClass) i.next();
-            System.out.println("teste"+ c.getURI() );
-        }
-        
-//        Individual shoe0 = model.createIndividual(NamedSpace + "shoe0", pants);
-//        Property hasSize = model.getProperty("Size");
-//        Property hasColour = model.getProperty("Colour");
-//        shoe0.addLiteral(hasSize, "45");
-//        shoe0.addLiteral(hasColour, "blacknigga");
-//
-//
-//
-//        System.out.println("teste: " + shoe0.getPropertyValue(hasSize));
     }
 
     public static void main(String args[]) {

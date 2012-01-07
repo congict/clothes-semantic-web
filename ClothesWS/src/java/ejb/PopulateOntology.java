de397ec8b6848fafb1b05027f7c14330bc344668
Nuno Khan <nunok7@gmail.com>
2012-01-07 16:40:51 +0000

    new libs for jena

--- ClothesWS/src/java/ejb/PopulateOntology.java
+++ ClothesWS/src/java/ejb/PopulateOntology.java
@@ -4,10 +4,22 @@
  */
 package ejb;
 
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
 /**
  *
  * @author psychok7
  */
 public class PopulateOntology {
-    
+
+    public PopulateOntology() {
+    }
+
+    private void init() {
+        Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        model.read("file:movieontology.owl");
+
+    }
 }

848192c309e92383613672fc4ebe2d7f7c6fbd14
Nuno Khan <nunok7@gmail.com>
2012-01-07 16:25:24 +0000

    new class added

---
+++ ClothesWS/src/java/ejb/PopulateOntology.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+/**
+ *
+ * @author psychok7
+ */
+public class PopulateOntology {
+    
+}

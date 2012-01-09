789778c2359f1c62ad904dae4b1c57d88aa85b58
leandro <leandro@leandro>
2012-01-09 05:07:07 +0000

    SPARQL + SQL search more queries

--- ClothesWS/src/java/util/SparqlHandler.java
+++ ClothesWS/src/java/util/SparqlHandler.java
@@ -1,7 +1,5 @@
 package util;
 
-import com.hp.hpl.jena.graph.Graph;
-import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.Query;
@@ -10,24 +8,12 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 import dao.SearchDAO;
 import entities.Clothes;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class SparqlHandler {
 
@@ -36,45 +22,152 @@ public class SparqlHandler {
     public ArrayList<Clothes> searchReults = new ArrayList<Clothes>();
 
     public SparqlHandler() {
-        //search(null);
+        search("big pants for woman");
     }
 
     public void search(String pattern) {
+        pattern.trim();
 
         // Create an empty in-memory model and populate it from the graph
         model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
         model.read("file:clothes.owl");
 
-        
-        if (pattern.equalsIgnoreCase("big shoes")){
-            Query query = null;    
+
+        if (pattern.equalsIgnoreCase("big shoes")) {
+            Query query = null;
             String bigShoes =
-                "PREFIX c: <http://www.clothes.pt/ontologies/clothes.owl#> "
-                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-                + "PREFIX rdfs:	<http://www.w3.org/2000/01/rdf-schema#> "
-                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
-                + "SELECT ?clothes ?size ?data ?id "
-                + " WHERE {"
-                + " ?clothes c:SQL_ID_DATA ?id . "
-                + " ?clothes c:hasSize ?size . "
-                + " ?size rdf:type c:Size . "
-                + " ?size c:Size_Data ?data . "
-                + " FILTER(?data > 42) . "
-                + " ?clothes rdf:type ?type . "
-                + " ?type rdfs:subClassOf* c:Shoes . "
-                + "  }";
+                    "PREFIX c: <http://www.clothes.pt/ontologies/clothes.owl#> "
+                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                    + "PREFIX rdfs:	<http://www.w3.org/2000/01/rdf-schema#> "
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+                    + "SELECT ?clothes ?size ?data ?id "
+                    + " WHERE {"
+                    + " ?clothes c:SQL_ID_DATA ?id . "
+                    + " ?clothes c:hasSize ?size . "
+                    + " ?size rdf:type c:Size . "
+                    + " ?size c:Size_Data ?data . "
+                    + " FILTER(?data > 42) . "
+                    + " ?clothes rdf:type ?type . "
+                    + " ?type rdfs:subClassOf* c:Shoes . "
+                    + "  }";
 
             query = QueryFactory.create(bigShoes);
-              // Execute the query and obtain results
+            // Execute the query and obtain results
             QueryExecution qe = QueryExecutionFactory.create(query, model);
             ResultSet results = qe.execSelect();
-            this.searchReults=sendResults(results);
+            this.searchReults = sendResults(results);
             qe.close();
         }
-        
-        // Output query results	
-        //ResultSetFormatter.out(System.out, results, query);
-       
+
+        if (pattern.equalsIgnoreCase("big pants for woman")) {
+            Query query = null;
+            String stringQuery =
+                    "PREFIX c: <http://www.clothes.pt/ontologies/clothes.owl#> "
+                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                    + "PREFIX rdfs:	<http://www.w3.org/2000/01/rdf-schema#> "
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+                    + "SELECT ?clothes ?size ?data ?id "
+                    + " WHERE {"
+                    + " ?clothes c:SQL_ID_DATA ?id . "
+                    + " ?clothes c:hasSize ?size . "
+                    + " ?size rdf:type c:Size . "
+                    + " ?size c:Size_Data ?data . "
+                    + " FILTER(?data > 42) . "
+                    + " ?clothes rdf:type ?type . "
+                    + " ?type rdfs:subClassOf* c:WomensJeans . "
+                    + "  }";
+
+            query = QueryFactory.create(stringQuery);
+            // Execute the query and obtain results
+            QueryExecution qe = QueryExecutionFactory.create(query, model);
+            ResultSet results = qe.execSelect();
+            this.searchReults = sendResults(results);
+            qe.close();
+        }
+
+
+        if (pattern.equalsIgnoreCase("small pants for man")) {
+            Query query = null;
+            String stringQuery =
+                    "PREFIX c: <http://www.clothes.pt/ontologies/clothes.owl#> "
+                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                    + "PREFIX rdfs:	<http://www.w3.org/2000/01/rdf-schema#> "
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+                    + "SELECT ?clothes ?size ?data ?id "
+                    + " WHERE {"
+                    + " ?clothes c:SQL_ID_DATA ?id . "
+                    + " ?clothes c:hasSize ?size . "
+                    + " ?size rdf:type c:Size . "
+                    + " ?size c:Size_Data ?data . "
+                    + " FILTER(?data < 40) . "
+                    + " ?clothes rdf:type ?type . "
+                    + " ?type rdfs:subClassOf* c:WensJeans . "
+                    + "  }";
+
+            query = QueryFactory.create(stringQuery);
+            // Execute the query and obtain results
+            QueryExecution qe = QueryExecutionFactory.create(query, model);
+            ResultSet results = qe.execSelect();
+            this.searchReults = sendResults(results);
+            qe.close();
+        }
+
+
+        if (pattern.equalsIgnoreCase("small shoes for man")) {
+            Query query = null;
+            String stringQuery =
+                    "PREFIX c: <http://www.clothes.pt/ontologies/clothes.owl#> "
+                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                    + "PREFIX rdfs:	<http://www.w3.org/2000/01/rdf-schema#> "
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+                    + "SELECT ?clothes ?size ?data ?id "
+                    + " WHERE {"
+                    + " ?clothes c:SQL_ID_DATA ?id . "
+                    + " ?clothes c:hasSize ?size . "
+                    + " ?size rdf:type c:Size . "
+                    + " ?size c:Size_Data ?data . "
+                    + " FILTER(?data < 40) . "
+                    + " ?clothes rdf:type ?type . "
+                    + " ?type rdfs:subClassOf* c:WensJeans . "
+                    + "  }";
+
+            query = QueryFactory.create(stringQuery);
+            // Execute the query and obtain results
+            QueryExecution qe = QueryExecutionFactory.create(query, model);
+            ResultSet results = qe.execSelect();
+            this.searchReults = sendResults(results);
+            qe.close();
+        }
+
+
+
+        for (int i = 0; i < this.searchReults.size(); i++) {
+            System.out.println("Clothes: " + this.searchReults.get(i).getModel());
+        }
+
+    }
+
+    public ArrayList<Clothes> sendResults(ResultSet results) {
+        ArrayList<Clothes> clothes = new ArrayList<Clothes>();
+        while (results.hasNext()) {
+            QuerySolution nextSolution = results.nextSolution();
+            Iterator<String> varNames = nextSolution.varNames();
+            RDFNode data = nextSolution.get("id");
+            if (data.isLiteral()) {
+                System.out.println("ID " + data.asLiteral().getString());
+                int id = Integer.parseInt(data.asLiteral().getString());
+                clothes.add(new SearchDAO().searchByID(id));
+            }
+        }
+        return clothes;
+    }
+
+    public static void main(String args[]) {
+        SparqlHandler sparqlHandler = new SparqlHandler();
+    }
+}
+// Output query results	
+//ResultSetFormatter.out(System.out, results, query);
 //            while (varNames.hasNext()) {
 //                String next = varNames.next();
 //                System.out.println("data:  " + data.toString());
@@ -114,43 +207,12 @@ public class SparqlHandler {
 //
 //
 //            }
-        //    System.out.println();
+//    System.out.println();
+//System.out.println(model_data.toString());
+//            StmtIterator listProperties = nextSolution.get("clothes").asResource().listProperties();
+//            while (listProperties.hasNext()) {
+//                Statement nextStatement = listProperties.nextStatement();
+//                System.out.println(nextStatement);
+//
+//            }
 
-            //System.out.println(model_data.toString());
-            //            StmtIterator listProperties = nextSolution.get("clothes").asResource().listProperties();
-            //            while (listProperties.hasNext()) {
-            //                Statement nextStatement = listProperties.nextStatement();
-            //                System.out.println(nextStatement);
-            //
-            //            }
-
-
-
-
-
-
-
-        // Important - free up resources used running the query
-        
-
-    }
-    
-    public ArrayList<Clothes> sendResults(ResultSet results){
-        ArrayList<Clothes> clothes = new ArrayList<Clothes>();
-        while (results.hasNext()) {
-            QuerySolution nextSolution = results.nextSolution();
-            Iterator<String> varNames = nextSolution.varNames();
-            RDFNode data = nextSolution.get("id");
-            if (data.isLiteral()) {
-                System.out.println("ID " + data.asLiteral().getString());
-                int id = Integer.parseInt(data.asLiteral().getString());
-                clothes.add(new SearchDAO().searchByID(id));
-            }
-        }
-        return clothes;
-    }
-
-    public static void main(String args[]) {
-        SparqlHandler sparqlHandler = new SparqlHandler();
-    }
-}
